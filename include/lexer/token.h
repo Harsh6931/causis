68bd063c08237744f547fcf89582f60e9893278c
HARSHIT SINHA <harshitsinha2006@gmail.com>
2026-09-03 10:12:32 +0530

    lexer: token object & type and token print are included

---
+++ include/lexer/token.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <optional>   // REMOVE IT IF NOT NEEDED
+#include <string>
+
+namespace causis::lexer {
+
+enum class TokenType {
+    // Keywords
+    World,
+    Robot,
+    Target,
+    Obstacle,
+    Behavior,
+    Every,
+    Tick,
+    If,
+    Else,
+    At,
+    True,
+    False,
+
+    // Literals and names
+    Integer,
+    Identifier,
+
+    // Punctuation
+    LeftParen, // left paranthesis
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Semicolon,
+    Comma,
+
+    // Operators
+    Equal,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    EqualEqual,
+    BangEqual,  // !=
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Bang,  // !
+
+    EndOfFile,  // EOF
+};
+
+struct Token {  // AN OBJECT OF THIS CLASS IS TOKEN, I  WILL ALLOT IT TYPE AND LEXEME & POSIITON
+    TokenType type{};   // kind of token
+    std::string lexeme;   // which text produced it
+    int line{1};     // where
+    int column{1};
+    int int_value{0};   // value of the token
+};
+//printing display helper i created
+// i created for GoogleTest, not for Lexer Algorithm
+
+// function declaration mapped to token.cpp
+const char* token_type_name(TokenType type);  // give token name in Human readable format  eg. TokenType::Robot = Robot
+std::string format_token(const Token& token); // shows entire token in readable format eg  Robot("ronny") at 1:7
+
+} // namespace causis::lexer
---
+++ src/lexer/token.cpp
@@ -0,0 +1,91 @@
+#include "lexer/token.h"
+
+namespace causis::lexer {
+
+const char* token_type_name(TokenType type) {
+    switch (type) {
+    case TokenType::World:
+        return "WORLD";
+    case TokenType::Robot:
+        return "ROBOT";
+    case TokenType::Target:
+        return "TARGET";
+    case TokenType::Obstacle:
+        return "OBSTACLE";
+    case TokenType::Behavior:
+        return "BEHAVIOR";
+    case TokenType::Every:
+        return "EVERY";
+    case TokenType::Tick:
+        return "TICK";
+    case TokenType::If:
+        return "IF";
+    case TokenType::Else:
+        return "ELSE";
+    case TokenType::At:
+        return "AT";
+    case TokenType::True:
+        return "TRUE";
+    case TokenType::False:
+        return "FALSE";
+    case TokenType::Integer:
+        return "INTEGER";
+    case TokenType::Identifier:
+        return "IDENTIFIER";
+    case TokenType::LeftParen:
+        return "LEFT_PAREN";
+    case TokenType::RightParen:
+        return "RIGHT_PAREN";
+    case TokenType::LeftBrace:
+        return "LEFT_BRACE";
+    case TokenType::RightBrace:
+        return "RIGHT_BRACE";
+    case TokenType::Semicolon:
+        return "SEMICOLON";
+    case TokenType::Comma:
+        return "COMMA";
+    case TokenType::Equal:
+        return "EQUAL";
+    case TokenType::Plus:
+        return "PLUS";
+    case TokenType::Minus:
+        return "MINUS";
+    case TokenType::Star:
+        return "STAR";
+    case TokenType::Slash:
+        return "SLASH";
+    case TokenType::EqualEqual:
+        return "EQUAL_EQUAL";
+    case TokenType::BangEqual:
+        return "BANG_EQUAL";
+    case TokenType::Less:
+        return "LESS";
+    case TokenType::LessEqual:
+        return "LESS_EQUAL";
+    case TokenType::Greater:
+        return "GREATER";
+    case TokenType::GreaterEqual:
+        return "GREATER_EQUAL";
+    case TokenType::Bang:
+        return "BANG";
+    case TokenType::EndOfFile:
+        return "END_OF_FILE";
+    }
+    return "UNKNOWN";
+}
+
+std::string format_token(const Token& token) {
+    const char* name = token_type_name(token.type);
+
+    if (token.type == TokenType::Integer) {
+        return std::string(name) + "(" + std::to_string(token.int_value) + ")";
+    }
+
+    if (token.type == TokenType::Identifier) {
+        return std::string(name) + "(" + token.lexeme + ")";
+    }
+
+    return name;
+}
+
+} // namespace causis::lexer
